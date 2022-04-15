883672866fbf7ffd3ba0f184d45861bd9399d7c6
Md-Sefat-Alam <md.sefatalam@gmail.com>
2022-04-15 11:07:05 +0600

    review

--- src/components/Dashboard/UserDashboard/Review/Review.js
+++ src/components/Dashboard/UserDashboard/Review/Review.js
@@ -1,7 +1,72 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import SectionHeader from "../../../shared/SectionHeader/SectionHeader";
+import { Button, TextField } from "@mui/material";
+import useAuth from "../../../../hooks/useAuth";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 const Review = () => {
-  return <div>Review page</div>;
+  const handleReviewTextRef = useRef();
+  const [value, setValue] = React.useState(4);
+  const { user, setMessage } = useAuth();
+  const history = useHistory();
+  const handleAddReview = () => {
+    const reviewData = {
+      email: user.email,
+      rate: value,
+      text: handleReviewTextRef.current.lastChild.firstChild.value,
+      date: new Date().toLocaleDateString(),
+    };
+    axios
+      .post(`http://localhost:5000/review`, reviewData)
+      .then((res) => {
+        if (res.status === 200) {
+          setMessage("Thanks for your review");
+          window.alert("added");
+          history.push("/dashboard/my-orders");
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <SectionHeader text={"Add your Review"} />
+      </div>
+      <Box
+        sx={{
+          "& > legend": { mt: 2 },
+        }}
+      >
+        <Rating
+          size="large"
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Box>
+      <div style={{ minWidth: "400px" }}>
+        <TextField
+          id="outlined-multiline-static"
+          label="Review Text In Details"
+          multiline
+          style={{ minWidth: "500px", margin: "10px" }}
+          rows={8}
+          ref={handleReviewTextRef}
+          defaultValue="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda enim quisquam ipsa similique quo temporibus exercitationem, doloribus dolorum id, molestiae suscipit itaque! Fuga, repellendus cumque ratione excepturi quos aperiam laudantium vitae officiis! Totam dolorum recusandae beatae sapiente non, voluptate architecto?"
+        />
+      </div>
+      <div style={{ minWidth: "500px", margin: "10px" }}>
+        <Button onClick={handleAddReview} align="right" variant="outlined">
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,14 +4,27 @@ import SectionHeader from "../shared/SectionHeader/SectionHeader";
 import Slider from "../Slider/Slider";
 import ServiceCard from "../shared/ServiceCard/ServiceCard";
 import useAuth from "../../hooks/useAuth";
+import axios from "axios";
 
 const Home = () => {
   const { products, setProducts } = useAuth();
+  const [review, setReview] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/products6")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/review`)
+      .then((res) => {
+        if (res.status === 200) {
+          setReview(res.data);
+        }
+      })
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(review);
   return (
     <div className="pageRoot">
       <Slider></Slider>
@@ -30,39 +43,37 @@ const Home = () => {
         <SectionHeader text={"Reviews"} />
         <Container maxWidth="lg">
           <div className="">
-            {[1, 2, 3, 4, 5, 6].map((n) => (
-              <div className="my-4 bg-gray-100 rounded p-1 flex">
-                <div className="flex items-center">
-                  <div className="flex justify-center items-center w-20 h-20 bg-gray-200 rounded-full">
-                    img
+            {review.length > 0 &&
+              review.map((n) => {
+                const { _id, email, rate, text, date } = n;
+                return (
+                  <div key={_id} className="my-4 bg-gray-100 rounded p-1 flex">
+                    <div className="flex items-center">
+                      <div className="flex justify-center uppercase items-center w-20 h-20 bg-gray-200 rounded-full">
+                        {email[0]}
+                      </div>
+                      <div
+                        style={{ minWidth: "200px" }}
+                        className="mx-3 flex flex-col"
+                      >
+                        <span className="text-gray-500">{date}</span>
+                        <h4 className="text-gray-900 font-bold">{email}</h4>
+                      </div>
+                    </div>
+                    <div className="bg-gray-100 flex-grow rounded pt-3 pl-1 pr-3 relative">
+                      <div className="absolute -top-3 left-2">
+                        <Rating
+                          name="half-rating-read"
+                          defaultValue={rate}
+                          precision={0.5}
+                          readOnly
+                        />
+                      </div>
+                      <div className="text-gray-500">{text}</div>
+                    </div>
                   </div>
-                  <div
-                    style={{ minWidth: "200px" }}
-                    className="mx-3 flex flex-col"
-                  >
-                    <span className="text-gray-500">20/02/2022</span>
-                    <h4 className="text-gray-900 font-bold">Md. Sefat Alam</h4>
-                  </div>
-                </div>
-                <div className="bg-gray-100 flex-grow rounded pt-3 pl-1 pr-3 relative">
-                  <div className="absolute -top-3 left-2">
-                    <Rating
-                      name="half-rating-read"
-                      defaultValue={4.5}
-                      precision={0.5}
-                      readOnly
-                    />
-                  </div>
-                  <div className="text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Voluptate sapiente non enim quia. Dignissimos, quidem
-                    possimus consequatur dolores animi neque. Dolorem, quisquam
-                    facere consequuntur itaque at consequatur maxime temporibus
-                    beatae.
-                  </div>
-                </div>
-              </div>
-            ))}
+                );
+              })}
           </div>
         </Container>
       </div>
